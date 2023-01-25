82986f416c1a4a05f49088d8bb089e3e2ded9e53
Al-noor N. Ali <79499719+Alnoor-Ali@users.noreply.github.com>
2023-01-25 11:24:15 +0800

    Initial Commit

---
+++ 8-Puzzle-Game/game/settings.py
@@ -0,0 +1,35 @@
+# Where all the constants goes
+
+title = '8 Puzzle Game'
+
+WHITE = (0, 0, 0)
+DARKGREY = (182, 141, 64) # def val sa button
+BGCOLOR = (40, 39, 36)
+TILE_COLOR = (207, 207, 207)
+
+WIDTH = 550
+HEIGHT = 700
+FPS = 10
+TILESIZE = 130
+GAME_SIZE = 3
+NAV_HEIGHT = 54
+TOP_MARGIN =0.6
+LEFT_MARGIN = 0.6
+BUTTON_WIDTH = TILESIZE
+BUTTON_HEIGHT = TILESIZE / 1
+
+STATIC = 'game/static/'
+LOGO = 'game/static/logo.png'
+SHUFFLE_BTN = 'game/static/shuffle-button.png'
+SOLVE_BTN = 'game/static/solve-button.png'
+SOLVED_BTN = 'game/static/solved-button.png'
+SHOW_BTN = 'game/static/show-button.png'
+HELP_BTN = 'game/static/help-button.png'
+SOLVING_IMG = 'game/static/solving.png'
+HELP_TEXT = 'game/static/help-text.png'
+ASTAR_BTN = 'game/static/astar-not-selected.png'
+BFS_BTN = 'game/static/bfs-not-selected.png'
+
+
+FONT_STYLE = 'Fugaz One'
+FONT_SIZE = 50
